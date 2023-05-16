5dc2a65122fc049c96a1a5b7280834f6ac0f39d9
Scott Thomas <sthomas@idis.com>
2023-05-15 19:09:59 -0600

    initial push

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cli-wallet
+
 A command-line interface based Ethereum wallet with token and smart contract support
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2023 Scott Thomas, scott@idis.com
+
+*/
+package main
+
+import "github.com/daxchain-io/cli-wallet/cmd"
+
+func main() {
+	cmd.Execute()
+}
